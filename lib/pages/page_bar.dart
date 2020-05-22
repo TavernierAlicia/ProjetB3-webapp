2cc2d385a652b27b2452adeb45417811776967bd
Elynad <meyer.mathieu-dev@mailo.com>
2020-05-22 08:23:13 +0200

    Fixed ListView overlap, changing items design

--- lib/pages/page_bar.dart
+++ lib/pages/page_bar.dart
@@ -35,49 +35,54 @@ class _PageBarState extends State<PageBar> {
     var _bar = widget.bar ;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_bar.name),
-      ),
-      body: _body(_bar),
-    );
-  }
-
-  Widget _body(Bar bar) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Image.network(bar.imageUrl),
-          _offer(bar),
-        ],
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(_bar.name,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      )),
+                  background: Image.network(
+                    "https://s1.qwant.com/thumbr/0x380/1/2/2a39f1f558f2cbbec11a08e43493bde861d612add6be643dbc5ad6fe0b16fc/BDBE5B56-B1B4-586D-27C8A70A4A54E50A.jpg?u=https%3A%2F%2Fimages.bwwstatic.com%2Fcolumnpic6%2FBDBE5B56-B1B4-586D-27C8A70A4A54E50A.jpg&q=0&b=1&p=0&a=1",
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          ];
+        },
+        body: _offer(_bar),
       ),
     );
   }
 
   /// Displays the section that handles the offers list.
   Widget _offer(Bar bar) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Container(
-        child: Column(
-          children: <Widget>[
-            Text(
-              "Our offer",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20
-              ),
-            ),
-            Padding(padding: EdgeInsets.all(20)),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: _productsList.length,
-              itemBuilder: (context, i) {
-                return productItem(context, _productsList[i]);
-              },
-            )
-          ],
+    return Column(
+      children: <Widget>[
+        Text(
+          "Our offer",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+          ),
         ),
-      ),
+        Padding(padding: EdgeInsets.all(20)),
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: _productsList.length,
+            itemBuilder: (context, i) {
+              return productItem(context, _productsList[i]);
+            },
+          ),
+        )
+      ],
     );
   }
 
--- lib/views/product_item.dart
+++ lib/views/product_item.dart
@@ -2,12 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:projet_b3/model/product.dart';
 
 Widget productItem(context, Product item) {
-  return Padding(
-    padding: EdgeInsets.all(20),
-    child: Row(
-      children: <Widget>[
-        Text(item.name),
-      ],
-    ),
+
+  double _screenWidth = MediaQuery.of(context).size.width ;
+
+  return StatefulBuilder(
+    builder: (BuildContext itemContext, StateSetter itemSetState) {
+      return Padding(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: _screenWidth / 2.5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    item.name.toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.all(3),),
+                  Text(
+                    item.description,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.all(3),),
+                  Text(
+                    item.price.toString(),
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: _screenWidth / 2.5,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: (() {
+                      print("Should remove item");
+                      if (item.quantity > 0)
+                        itemSetState(() => item.quantity--);
+                    }),
+                    child: Image.asset("assets/remove.png"),
+                  ),
+                  Text(
+                    item.quantity.toString(),
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: (() {
+                      print("Should add item");
+                      itemSetState(() => item.quantity++) ;
+                    }),
+                    child: Image.asset("assets/add.png"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    },
   );
 }
