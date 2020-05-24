e567eead9e1522130e3e1f1db48f8a13bc10f5b0
Elynad <meyer.mathieu-dev@mailo.com>
2020-05-24 15:36:46 +0200

    Page cart summary done

--- lib/pages/page_cart.dart
+++ lib/pages/page_cart.dart
@@ -24,6 +24,12 @@ class _PageCartState extends State<PageCart> {
   double          _screenWidth = 0 ;
   UserPosition    _currentUserPosition = UserPosition.unspecified ;
 
+  static const String DEFAULT_ARRIVING_IN_TEXT = "J'arrive dans quelques minutes" ;
+
+  bool            _showTimeList = false ;
+  var             _arrivingInList = [10, 15, 20, 25, 30] ;
+  var             _arrivingInText = DEFAULT_ARRIVING_IN_TEXT ;
+
   @override
   Widget build(BuildContext context) {
 
@@ -45,6 +51,25 @@ class _PageCartState extends State<PageCart> {
         ),
       ),
       body: _body(),
+      bottomSheet: InkWell(
+        onTap: null,
+        child: Container(
+          width: _screenWidth,
+          color: Colors.deepOrange,
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Valider",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 
@@ -59,12 +84,108 @@ class _PageCartState extends State<PageCart> {
           _displayTotal(),
           Padding(padding: EdgeInsets.all(10),),
           _userPosition(),
+          Padding(padding: EdgeInsets.all(10),),
+          _tips(),
+          Padding(padding: EdgeInsets.all(10),),
+          _paymentMethod(),
+          Padding(padding: EdgeInsets.all(40),)
         ],
       ),
     );
   }
 
+  Widget    _paymentMethod() {
+    return Column(
+      children: <Widget>[
+        Text("Choisissez un autre moyen de paiement"),
+        Padding(padding: EdgeInsets.all(10),),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Image.asset("assets/cb.png"),
+            Image.asset("assets/paypal.png"),
+            Image.asset("assets/money.png"),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget    _tips() {
+
+    return Container(
+      color: Colors.deepOrangeAccent.withOpacity(0.4),
+      width: _screenWidth,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Text("Je souhaite laisser un pourboire"),
+            Padding(padding: EdgeInsets.all(10),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepOrange
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "1 €",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.deepOrange
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "2 €",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.deepOrange
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "3 €",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+
   Widget    _userPosition() {
+
     // TODO : Display items if the user clicks on something
     return Padding(
       padding: EdgeInsets.only(left: 30, right: 30),
@@ -74,15 +195,19 @@ class _PageCartState extends State<PageCart> {
             onTap: (() {
               setState(() {
                 _currentUserPosition = UserPosition.onTheSpot ;
+                _showTimeList = false ;
+                _arrivingInText = DEFAULT_ARRIVING_IN_TEXT ;
               });
             }),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Radio(
-                  groupValue: _currentUserPosition,
+                  groupValue: _currentUserPosition.index,
                   value: UserPosition.onTheSpot.index,
-                  onChanged: ((newValue){}),
+                  onChanged: ((newValue){
+                    setState(() {});
+                  }),
                 ),
                 Text(
                   "Je suis sur place",
@@ -97,18 +222,21 @@ class _PageCartState extends State<PageCart> {
             onTap: (() {
               setState(() {
                 _currentUserPosition = UserPosition.onMyWay ;
+                _showTimeList = true ;
               });
             }),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Radio(
-                  groupValue: _currentUserPosition,
+                  groupValue: _currentUserPosition.index,
                   value: UserPosition.onMyWay.index,
-                  onChanged: ((newValue){}),
+                  onChanged: ((newValue){
+                    setState(() {});
+                  }),
                 ),
                 Text(
-                  "J'arrive dans quelques minutes",
+                  _arrivingInText,
                   style: TextStyle(
                     fontSize: 15,
                   ),
@@ -117,9 +245,34 @@ class _PageCartState extends State<PageCart> {
             ),
           ),
           Container(
-            child: (_currentUserPosition == UserPosition.onMyWay)
-                ? Wrap(
-              
+            child: (_showTimeList)
+                ? Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  style: BorderStyle.solid,
+                  width: 1,
+                )
+              ),
+              child: DropdownButton(
+                isExpanded: true,
+                hint: Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text("Combien de minutes"),
+                ),
+                onChanged: ((newValue) {
+                  setState(() {
+                    _arrivingInText = "J'arrive dans $newValue minutes";
+                    _showTimeList = false ;
+                  });
+                }),
+                items: _arrivingInList.map((int value) {
+                  return DropdownMenuItem<int>(
+                    value: value,
+                    child: Text("$value minutes"),
+                  );
+                }).toList(),
+              ),
             )
                 : null,
           ),
@@ -136,7 +289,7 @@ class _PageCartState extends State<PageCart> {
           Text(
             "Votre commande",
             style: TextStyle(
-              fontSize: 15
+                fontSize: 15
             ),
           ),
           Padding(padding: EdgeInsets.all(15),),
